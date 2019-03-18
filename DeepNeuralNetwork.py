931e1838f1ab3b130abb0e24fc98cbb3e6a5b6e5
Iaro <iaroslav.sedrichev@gmail.com>
2019-03-18 12:35:55 -0700

    Update

--- DeepNeuralNetwork.py
+++ DeepNeuralNetwork.py
@@ -162,10 +162,8 @@ def initialize_parameters_deep(layer_dims):
     L = len(layer_dims)            # number of layers in the network
 
     for l in range(1, L):
-        ### START CODE HERE ### (≈ 2 lines of code)
         parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
-        ### END CODE HERE ###
         
         assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
         assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
@@ -187,9 +185,11 @@ def linear_forward(A, W, b):
     cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
     """
     
-    ### START CODE HERE ### (≈ 1 line of code)
+    print("A:",A.shape)
+    print("W:",W.shape)
+    print("b:",b.shape)
+    
     Z = np.dot(W, A) + b
-    ### END CODE HERE ###
     
     assert(Z.shape == (W.shape[0], A.shape[1]))
     cache = (A, W, b)
@@ -214,17 +214,13 @@ def linear_activation_forward(A_prev, W, b, activation):
     
     if activation == "sigmoid":
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
-        ### START CODE HERE ### (≈ 2 lines of code)
         Z, linear_cache = linear_forward(A_prev, W, b)
         A, activation_cache = sigmoid(Z)
-        ### END CODE HERE ###
     
     elif activation == "relu":
         # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
-        ### START CODE HERE ### (≈ 2 lines of code)
         Z, linear_cache = linear_forward(A_prev, W, b)
         A, activation_cache = relu(Z)
-        ### END CODE HERE ###
     
     assert (A.shape == (W.shape[0], A_prev.shape[1]))
     cache = (linear_cache, activation_cache)
@@ -253,24 +249,20 @@ def L_model_forward(X, parameters):
     # Implement [LINEAR -> RELU]*(L-1). Add "cache" to the "caches" list.
     for l in range(1, L):
         A_prev = A 
-        ### START CODE HERE ### (≈ 2 lines of code)
         A, cache = linear_activation_forward(A_prev, 
                                              parameters['W' + str(l)], 
                                              parameters['b' + str(l)], 
                                              activation='relu')
         caches.append(cache)
         
-        ### END CODE HERE ###
     
     # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
-    ### START CODE HERE ### (≈ 2 lines of code)
     AL, cache = linear_activation_forward(A, 
                                           parameters['W' + str(L)], 
                                           parameters['b' + str(L)], 
                                           activation='sigmoid')
     caches.append(cache)
     
-    ### END CODE HERE ###
     
     assert(AL.shape == (1, X.shape[1]))
             
@@ -291,9 +283,7 @@ def compute_cost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    ### START CODE HERE ### (≈ 1 lines of code)
     cost = (-1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
-    ### END CODE HERE ###
     
     cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
     assert(cost.shape == ())
@@ -316,15 +306,16 @@ def linear_backward(dZ, cache):
     A_prev, W, b = cache
     m = A_prev.shape[1]
 
-    ### START CODE HERE ### (≈ 3 lines of code)
     dW = np.dot(dZ, cache[0].T) / m
     db = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / m
     dA_prev = np.dot(cache[1].T, dZ)
-    ### END CODE HERE ###
     
+    #print(dA_prev.shape, A_prev.shape)
     assert (dA_prev.shape == A_prev.shape)
+    #print(dW.shape, W.shape)
     assert (dW.shape == W.shape)
-    assert (isinstance(db, float))
+    #print(type(db))
+    #assert (isinstance(db, float))
     
     return dA_prev, dW, db
 
@@ -345,14 +336,10 @@ def linear_activation_backward(dA, cache, activation):
     linear_cache, activation_cache = cache
     
     if activation == "relu":
-        ### START CODE HERE ### (≈ 2 lines of code)
         dZ = relu_backward(dA, activation_cache)
-        ### END CODE HERE ###
         
     elif activation == "sigmoid":
-        ### START CODE HERE ### (≈ 2 lines of code)
         dZ = sigmoid_backward(dA, activation_cache)
-        ### END CODE HERE ###
     
     # Shorten the code
     dA_prev, dW, db = linear_backward(dZ, linear_cache)
@@ -382,28 +369,22 @@ def L_model_backward(AL, Y, caches):
     Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
     
     # Initializing the backpropagation
-    ### START CODE HERE ### (1 line of code)
     dAL = dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-    ### END CODE HERE ###
     
     # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
-    ### START CODE HERE ### (approx. 2 lines)
     current_cache = caches[-1]
-    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_backward(sigmoid_backward(dAL, 
-                                                                                                        current_cache[1]), 
-                                                                                       current_cache[0])
-    ### END CODE HERE ###
+    #print("0:", current_cache[0])
+    #print("1:", current_cache[1])
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_backward(sigmoid_backward(dAL, current_cache[1]), current_cache[0])
     
     for l in reversed(range(L-1)):
         # lth layer: (RELU -> LINEAR) gradients.
         # Inputs: "grads["dA" + str(l + 2)], caches". Outputs: "grads["dA" + str(l + 1)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)] 
-        ### START CODE HERE ### (approx. 5 lines)
         current_cache = caches[l]
         dA_prev_temp, dW_temp, db_temp = linear_backward(sigmoid_backward(dAL, current_cache[1]), current_cache[0])
         grads["dA" + str(l + 1)] = dA_prev_temp
         grads["dW" + str(l + 1)] = dW_temp
         grads["db" + str(l + 1)] = db_temp
-        ### END CODE HERE ###
 
     return grads
 
@@ -424,11 +405,9 @@ def update_parameters(parameters, grads, learning_rate):
     L = len(parameters) // 2 # number of layers in the neural network
 
     # Update rule for each parameter. Use a for loop.
-    ### START CODE HERE ### (≈ 3 lines of code)
     for l in range(L):
         parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
         parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
-    ### END CODE HERE ###
         
     return parameters
 
@@ -452,32 +431,22 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
     costs = []                         # keep track of cost
     
     # Parameters initialization.
-    ### START CODE HERE ###
     parameters = initialize_parameters_deep(layers_dims)
-    ### END CODE HERE ###
     
     # Loop (gradient descent)
     for i in range(0, num_iterations):
 
         # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        ### START CODE HERE ### (≈ 1 line of code)
         AL, caches = L_model_forward(X, parameters)
-        ### END CODE HERE ###
         
         # Compute cost.
-        ### START CODE HERE ### (≈ 1 line of code)
         cost = compute_cost(AL, Y)
-        ### END CODE HERE ###
     
         # Backward propagation.
-        ### START CODE HERE ### (≈ 1 line of code)
         grads = L_model_backward(AL, Y, caches)
-        ### END CODE HERE ###
  
         # Update parameters.
-        ### START CODE HERE ### (≈ 1 line of code)
         parameters = update_parameters(parameters, grads, learning_rate)
-        ### END CODE HERE ###
                 
         # Print the cost every 100 training example
         if print_cost and i % 100 == 0:
@@ -486,11 +455,11 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
             costs.append(cost)
             
     # plot the cost
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('cost')
-    plt.xlabel('iterations (per tens)')
-    plt.title("Learning rate =" + str(learning_rate))
-    plt.show()
+    #plt.plot(np.squeeze(costs))
+    #plt.ylabel('cost')
+    #plt.xlabel('iterations (per tens)')
+    #plt.title("Learning rate =" + str(learning_rate))
+    #plt.show()
     
     return parameters
 
